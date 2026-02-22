ea155c2a3ed695364d1a98d316dedebc9831dfae
Isaac <isdowdy@outlook.com>
2026-02-21 20:00:37 -0500

    Quick fix: Merging histogram scripts
    I've created one histogram.js file from the two that take as input which variable name to render - this'll be important later.
    
    I've also started on level 3; I've found quite a few more attributes, merged the new data into a new csv file that I've just called "data.csv". Lastly, I put together a couple of dropboxes that will be used to select attributes 1 and 2 - they are not currently functional beyond taking up space on the screen. That's next.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-let data, expenditureChart, expectancyChart, scatterplotChart, choroMap;
+let data, expenditureChart, expectancyChart, scatterplotChart, choroMap, attribute1, attribute2;
 
 Promise.all([
         d3.json('data/custom.geo.json'),
-        d3.csv('data/health-outcomes.csv', d => ({
+        d3.csv('data/data.csv', d => (
+        {
             Country: d.Entity,
             Code: d.Code,
             Expenditure: +d.Expenditure,
-            Expectancy: +d.Expectancy
+            Expectancy: +d.Expectancy,
+            Population: +d.Population,
+            GDP: +d.GDP,
+            Physicians: +d.Physicians,
+            Undernourishment: +d.Undernourishment,
+            Obesity: +d.Obesity,
+            WorkingHours: +d.WorkingHours
         }))
-    ]).then(_data => {
+    ]).then(_data => 
+        {
         data = _data;      
         console.log(data);
 
@@ -26,39 +34,75 @@ Promise.all([
 
 
         // Create healthcare expenditure histogram
-        expenditureChart = new expenditureHistogram ({
+        histogram1 = new Histogram (
+        {
             parentElement: '#barChart',
             containerWidth: 450,
             containerHeight: 200,
-            margin: { top: 20, right: 20, bottom: 60, left: 40 }
+            margin: { top: 20, right: 20, bottom: 60, left: 40 },
+            attribute: "Expenditure"
         }, data[1]);
 
         // Create life expectancy histogram
-        expectancyChart = new expectancyHistogram ({
+        histogram2 = new Histogram (
+        {
             parentElement: '#barChart2',
             containerWidth: 450,
             containerHeight: 200,
-            margin:  { top: 20, right: 20, bottom: 60, left: 40 }
+            margin:  { top: 20, right: 20, bottom: 60, left: 40 },
+            attribute: "Expectancy"
         }, data[1]);
 
         // Create life expectancy vs healthcare expenditure scatterplot
-        scatterplotChart = new scatterplot ({
+        scatterplotChart = new scatterplot (
+        {
             parentElement: '#scatterplot',
             containerWidth: 450,
             containerHeight: 259,
-            margin: { top: 20, right: 20, bottom: 60, left: 40 }
+            margin: { top: 20, right: 20, bottom: 60, left: 40 },
+            attribute1: "Expenditure",
+            atribute2: "Expectancy"
         }, data[1]);
 
-        choroMap1 = new choropleth ({
+        choroMap1 = new choropleth (
+        {
             parentElement: '#choropleth1',
             containerWidth: 600,
             containerHeight: 600,
         }, data[0], 'Expenditure');
 
-        choroMap2 = new choropleth ({
+        choroMap2 = new choropleth (
+        {
             parentElement: '#choropleth2',
             containerWidth: 600,
             containerHeight: 600,
         }, data[0], 'Expectancy');
 
     });
+
+function myFunction1() 
+{
+    document.getElementById("myDropdown1").classList.toggle("show");
+}
+
+function myFunction2() 
+{
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+window.onclick = function(event) 
+{
+    if (!event.target.matches('.dropbtn'))
+    {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++)
+        {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show'))
+            {
+                openDropdown.classList.remove("show");
+            }
+        }
+    }
+}
